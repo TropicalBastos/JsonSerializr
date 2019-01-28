2e81ad980170a8cd8a7a481240f9803ca31c459a
Ian Bastos <ian-bastos@live.com>
2019-01-28 03:10:48 +0000

    force fail test on error

--- src/test/java/SerializerTest.java
+++ src/test/java/SerializerTest.java
@@ -1,6 +1,8 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 import com.tropicalbastos.jsonserializr.*;
+import java.util.List;
+import java.util.ArrayList;
 
 public class SerializerTest {
 
@@ -12,20 +14,38 @@ public class SerializerTest {
             assertEquals("{\"name\":\"Steve\",\"age\":\"30\"}", json);
         }catch(JsonSerializeException e){
             System.out.println(e.getMessage());
+            fail();
         }
     }
 
-    @Test public void serializesClassField(){
+    @Test public void serializesClassField() {
         MockClass mc = new MockClass("Ian", 24);
         MockClassParent mockClassParent = new MockClassParent(mc);
         Serializer serializer = new Serializer();
         try{
             String json = serializer.serialize(mockClassParent);
             assertEquals("{\"person\":{\"name\":\"Ian\",\"age\":\"24\"}", json);
-        }catch(JsonSerializeException e){
+        }catch(JsonSerializeException e) {
             System.out.println(e.getMessage());
+            fail();
         }
 
+    }
+
+    @Test public void serializesLists() {
+        List<MockClass> people = new ArrayList();
+        people.add(new MockClass("Brienne", 40));
+        people.add(new MockClass("Harold", 27));
+        people.add(new MockClass("Hannah", 32));
+        Serializer serializer = new Serializer();
+        try {
+            String json = serializer.serialize(people);
+            assertEquals("[{\"name\":\"Brienne\",\"age\":\"40\"},{\"name\":\"Harold\",\"age\":\"27\"},{\"name\":\"Hannah\",\"age\":\"32\"}]", json);
+        } catch(JsonSerializeException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            fail();
+        }
     }
 
 }
